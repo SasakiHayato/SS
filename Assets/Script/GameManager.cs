640b14a0876bb39fe831f4004fd40cfd21f8119b
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-06-23 15:17:23 +0900

    初期コミット

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int encount;
+    private bool encountBool = true;
+
+    PlayerController player;
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        RandomEncounter();
+
+        if (encount < player.step)
+        {
+            Scene();
+        }
+    }
+
+    private void RandomEncounter()
+    {
+        if (encountBool)
+        {
+            encount = Random.Range(1, 3);
+            Debug.Log(encount);
+        }
+
+        encountBool = false;
+    }
+
+    public void Scene()
+    {
+        SceneManager.LoadScene("ButtleScene");
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float step;
+
+    GameManager manager;
+    void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(h / 16, v / 16, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            step += Time.deltaTime;
+            Debug.Log(step);
+        }
+    }
+}
